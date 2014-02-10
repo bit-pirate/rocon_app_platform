f8f2d75c6d70b7dab468f05cb48fe05800916cb2
Jihoon Lee <jihoonlee.in@gmail.com>
2014-02-10 12:55:46 +0900

    add doc. add init

--- rocon_app_utilities/setup.py
+++ rocon_app_utilities/setup.py
@@ -6,6 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rocon_app_utilities'],
     package_dir={'': 'src'},
+    scripts=['scripts/rapp'],
     requires=['roslib', 'rospy', 'rocon_utilities']
 )
 
---
+++ rocon_app_utilities/src/rocon_app_utilities/rapp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_app_platform/license/LICENSE
+#
+#################################################################################
+
+def main():
+    print "Hello"
